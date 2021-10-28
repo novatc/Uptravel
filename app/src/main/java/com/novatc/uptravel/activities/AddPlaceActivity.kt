fa0767c30611433a4854d800c9de79c7f4cc684b
Lucas Wagner <lucas.wagner@uni-oldenburg.de>
2021-10-28 21:05:26 +0200

    Firebase

--- app/src/main/java/com/novatc/uptravel/activities/AddPlaceActivity.kt
+++ app/src/main/java/com/novatc/uptravel/activities/AddPlaceActivity.kt
@@ -5,7 +5,10 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
+import android.graphics.Bitmap
 import android.icu.util.Calendar
 import android.net.Uri
 import android.os.Build
@@ -16,20 +19,28 @@ import android.provider.Settings
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.novatc.uptravel.R
+import com.novatc.uptravel.model.PlacesModel
 import kotlinx.android.synthetic.main.activity_add_place.*
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var dateSetLisener: DatePickerDialog.OnDateSetListener
+    val db = Firebase.firestore
 
     @RequiresApi(Build.VERSION_CODES.N)
     private var cal = Calendar.getInstance()
@@ -42,6 +53,8 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_place)
+
+        FirebaseApp.initializeApp(this)
 
         setSupportActionBar(toolbar_add_place)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -56,6 +69,7 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
             updateDateInView()
         }
+        updateDateInView()
         et_date.setOnClickListener(this)
         tv_add_image.setOnClickListener(this)
         btn_save.setOnClickListener(this)
@@ -116,6 +130,38 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         "Pls give it a location",
                         Toast.LENGTH_LONG
                     ).show()
+                    saveImageToInternalStorage == null -> Toast.makeText(
+                        this@AddPlaceActivity,
+                        "Pls take a picture",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    else -> {
+                        val placeModel = PlacesModel(
+                            0,
+                            et_title.text.toString(),
+                            saveImageToInternalStorage.toString(),
+                            et_description.text.toString(),
+                            et_date.text.toString(),
+                            et_location.text.toString(),
+                            mLatitude,
+                            mLongitude
+                        )
+                        db.collection("Places").add(placeModel)
+                            .addOnSuccessListener { documentReference ->
+                                Toast.makeText(
+                                    this@AddPlaceActivity,
+                                    "\"DocumentSnapshot added with ID: ${documentReference.id}",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }.addOnFailureListener { e ->
+                                Toast.makeText(
+                                    this@AddPlaceActivity,
+                                    "Error, "+e,
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                    }
+
                 }
             }
         }
@@ -170,6 +216,7 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         @Suppress("DEPRECATION")
                         val selectedImageBitmap =
                             MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                        saveImageToInternalStorage = saveImageToInternalStorage(selectedImageBitmap)
                         iv_place_image!!.setImageBitmap(selectedImageBitmap)
                     } catch (e: IOException) {
                         e.printStackTrace()
@@ -179,7 +226,43 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun saveImageToInternalStorage(bitmap: Bitmap): Uri {
+
+        // ContextWrapper Instanz erstellen
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Neue Datei (File) initialisieren
+        // getDir gibt Pfad zum internen Speicherort zurück
+        /**
+         * MODE_PRIVATE: Datei kann nur durch App (oder Apps, die die selbe Nutzer ID haben)
+         * aufgerufen werden.
+         */
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+
+        // Datei (File) erstellen, um das Foto zu speichern
+        file = File(file, "${UUID.randomUUID()}.jpg")
+
+        try {
+            // FileOutputStream der Datei in Variable speichern
+            val stream: OutputStream = FileOutputStream(file)
+
+            // Bitmap zum Speichern "verpacken" (Compress bitmap)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+
+            stream.flush()
+
+            // Stream schließen
+            stream.close()
+        } catch (e: IOException) { // Ausnahme abfangen und ausgeben
+            e.printStackTrace()
+        }
+
+        // URI des gespeicherten Fotos zurückgeben
+        return Uri.parse(file.absolutePath)
+    }
+
     companion object {
         private const val GALLERY = 1
+        private const val IMAGE_DIRECTORY = "UpTravel"
     }
 }
